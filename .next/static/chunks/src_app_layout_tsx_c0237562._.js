4fd6ee92f47075dee2b404f43080fb753067d835
jasim-techie <jasimbro25@gmail.com>
2025-06-11 20:23:37 +0530

    Update .gitignore to ignore env, build, and log files

--- .next/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_globals_b805903d.css",
-  "static/chunks/node_modules_c4732a9f._.js",
-  "static/chunks/src_5e1e5ea1._.js"
-],
-    source: "dynamic"
-});
